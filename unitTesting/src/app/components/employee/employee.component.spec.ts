14ca2bf6d9c123ab4c9bf70e11a3f139c791ed72
Santosh Bujala <bujalasanthoshreddy@gmail.com>
2021-08-21 20:41:05 +0530

    TestBed with SpyOn

--- unitTesting/src/app/components/employee/employee.component.spec.ts
+++ unitTesting/src/app/components/employee/employee.component.spec.ts
@@ -1,10 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AuthenticationService } from 'src/app/services/authentication.service';
 
 import { EmployeeComponent } from './employee.component';
 
 describe('EmployeeComponent', () => {
   let component: EmployeeComponent;
   let fixture: ComponentFixture<EmployeeComponent>;
+  let authService: AuthenticationService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -16,10 +18,19 @@ describe('EmployeeComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(EmployeeComponent);
     component = fixture.componentInstance;
+    authService = TestBed.inject(AuthenticationService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  it('should return Authentication Check Called', () => {
+    spyOn(authService, 'checkAuthenticate').and.returnValue(true);
+    let salSlip = component.getSalarySlip()
+    expect(salSlip).toEqual('Salary Slip')
+    expect(authService.checkAuthenticate).toHaveBeenCalled();
+  });
+  
+
 });
